545dc8d0d46da36d1cac682f56ab16f8bb441622
Sathik <architect@arivelm.com>
2022-12-08 15:04:07 +0530

    intial

---
+++ BindTextFileToGrid/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace BindTextFileToGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addRowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSNO = new System.Windows.Forms.TextBox();
+            this.txtFN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxG = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.statusCheckBox = new System.Windows.Forms.CheckBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(881, 240);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addRowToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(881, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addRowToolStripMenuItem
+            // 
+            this.addRowToolStripMenuItem.Name = "addRowToolStripMenuItem";
+            this.addRowToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.addRowToolStripMenuItem.Text = "Add Row";
+            this.addRowToolStripMenuItem.Click += new System.EventHandler(this.addRowToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.statusCheckBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.comboBoxG);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtLN);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtFN);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtSNO);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(881, 183);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selected Record";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "S.No";
+            // 
+            // txtSNO
+            // 
+            this.txtSNO.Location = new System.Drawing.Point(126, 27);
+            this.txtSNO.Name = "txtSNO";
+            this.txtSNO.Size = new System.Drawing.Size(100, 23);
+            this.txtSNO.TabIndex = 1;
+            // 
+            // txtFN
+            // 
+            this.txtFN.Location = new System.Drawing.Point(126, 62);
+            this.txtFN.Name = "txtFN";
+            this.txtFN.Size = new System.Drawing.Size(210, 23);
+            this.txtFN.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name";
+            // 
+            // txtLN
+            // 
+            this.txtLN.Location = new System.Drawing.Point(476, 59);
+            this.txtLN.Name = "txtLN";
+            this.txtLN.Size = new System.Drawing.Size(208, 23);
+            this.txtLN.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(370, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(126, 101);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 23);
+            this.txtAge.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Gender";
+            // 
+            // comboBoxG
+            // 
+            this.comboBoxG.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxG.FormattingEnabled = true;
+            this.comboBoxG.Items.AddRange(new object[] {
+            "Female",
+            "Male",
+            "Not Sepcify"});
+            this.comboBoxG.Location = new System.Drawing.Point(126, 138);
+            this.comboBoxG.Name = "comboBoxG";
+            this.comboBoxG.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxG.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(370, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Status";
+            // 
+            // statusCheckBox
+            // 
+            this.statusCheckBox.AutoSize = true;
+            this.statusCheckBox.Location = new System.Drawing.Point(476, 27);
+            this.statusCheckBox.Name = "statusCheckBox";
+            this.statusCheckBox.Size = new System.Drawing.Size(15, 14);
+            this.statusCheckBox.TabIndex = 2;
+            this.statusCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(778, 139);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bind Text File To Grid";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addRowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSNO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxG;
+        private System.Windows.Forms.CheckBox statusCheckBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ BindTextFileToGrid/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BindTextFileToGrid
+{
+    public partial class Form1 : Form
+    {
+        int rowIX = -1;
+        bool isNewRow=false, isDelete=false;
+        string filePath = "txtFile.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createTxtFile();
+            newRow();
+            
+            DataTable dataTable = ConvertToDataTable(6);
+              dataGridView1.DataSource = dataTable;
+            groupBox1.Enabled= false;
+        }
+        private void createTxtFile()
+        {
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter w = File.AppendText(filePath))
+                {
+                    w.WriteLine("S.No,Status,First Name,Last Name,Age,Gender");
+                    w.Flush();
+                    w.Close();
+                }
+            }
+        }
+
+        private void clearForm()
+        {
+            txtSNO.Text = "";
+            statusCheckBox.Checked = false;
+            txtFN.Text = "";
+            txtLN.Text = "";
+            txtAge.Text = "";
+            comboBoxG.SelectedIndex=-1;
+        }
+        private void newRow()
+        {
+            addRowToolStripMenuItem.Enabled = true;
+            saveToolStripMenuItem.Enabled = false;
+            deleteToolStripMenuItem.Enabled = false;
+            
+        }
+
+        private void saveRow()
+        {
+            addRowToolStripMenuItem.Enabled = false;
+            saveToolStripMenuItem.Enabled = true;
+            deleteToolStripMenuItem.Enabled = false;
+           
+        }
+
+        private void modieRow()
+        {
+            addRowToolStripMenuItem.Enabled = false;
+            saveToolStripMenuItem.Enabled = true;
+            deleteToolStripMenuItem.Enabled = true;
+           
+        }
+
+        private void addRowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+            isNewRow= true;
+            txtSNO.Focus();
+          saveRow(); 
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = false;
+           
+            lineChanger(txtSNO.Text.Trim() + "," + statusCheckBox.Checked + "," +txtFN.Text + "," +txtLN.Text + "," +txtAge.Text + "," +comboBoxG.SelectedItem);
+            dataGridView1.DataSource = ConvertToDataTable(6);
+            dataGridView1.Update();
+            dataGridView1.Refresh();
+            clearForm();
+            newRow();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = false; isDelete = true;
+            lineChanger("", isDelete);
+            newRow();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex>=0)
+            {
+                rowIX=e.RowIndex;
+                loadValues();
+                modieRow();
+            }
+        }
+
+        private void loadValues()
+        {
+            txtSNO.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            statusCheckBox.Checked =Convert.ToBoolean( dataGridView1.SelectedRows[0].Cells[1].Value.ToString());
+            txtFN.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtLN.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txtAge.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            comboBoxG.SelectedItem = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+
+        private DataTable ConvertToDataTable(int numberOfColumns)
+        {
+            DataTable tbl = new DataTable();
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            for (int col = 0; col < numberOfColumns; col++)
+            {
+                var cols = lines[0].Split(',');
+                tbl.Columns.Add(new DataColumn(cols[col].ToString()));
+            }
+
+            lines = lines.Skip(1).ToArray();
+            foreach (string line in lines)
+            {
+
+                var cols = line.Split(',');
+                DataRow dr = tbl.NewRow();
+                for (int cIndex = 0; cIndex < cols.Length; cIndex++)
+                {
+                    dr[cIndex] = cols[cIndex];
+                }
+
+                tbl.Rows.Add(dr);
+            }
+
+            return tbl;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void lineChanger(string newText, bool isDelete = false)
+        {
+            string[] arrLine = File.ReadAllLines(filePath);
+            if (isNewRow)
+            {
+                Array.Resize<string>(ref arrLine, arrLine.Length + 1);
+                arrLine[arrLine.Length - 1] = newText;
+            }
+            else if (isDelete)
+            {
+                string fileIN = filePath;
+                string fileOUT = "temp.txt";
+
+                for (int i = 0; i < arrLine.Length; i++)
+                    if (!(rowIX + 1 == i))
+                    {
+                        File.AppendAllText(fileOUT, arrLine[i]);
+                        File.AppendAllText(fileOUT, Environment.NewLine);
+                    }
+                File.Delete(fileIN);
+                File.Move(fileOUT, fileIN);
+
+            }
+            else
+            {
+                arrLine[rowIX + 1] = newText;
+            }
+            if (!isDelete)
+                File.WriteAllLines(filePath, arrLine);
+        }
+    }
+}
